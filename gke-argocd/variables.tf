a67667beaa2b92a384de23d3a8b39bd5466a7af1
Jose Asuncion <jeunito@gmail.com>
2022-11-04 21:03:00 -0700

    change name

--- gke-argocd/variables.tf
+++ gke-argocd/variables.tf
@@ -4,7 +4,7 @@ variable "project_id" {
 
 variable "cluster_name" {
   description = "The name for the GKE cluster"
-  default     = "gke-on-vpc-cluster"
+  default     = "gke-argocd"
 }
 
 variable "region" {
@@ -14,21 +14,21 @@ variable "region" {
 
 variable "network" {
   description = "The VPC network created to host the cluster in"
-  default     = "gke-network"
+  default     = "gke-argocd-network"
 }
 
 variable "subnetwork" {
   description = "The subnetwork created to host the cluster in"
-  default     = "gke-subnet"
+  default     = "gke-argocd-subnet"
 }
 
 variable "ip_range_pods_name" {
   description = "The secondary ip range to use for pods"
-  default     = "ip-range-pods"
+  default     = "gke-argocd-ip-range-pods"
 }
 
 variable "ip_range_services_name" {
   description = "The secondary ip range to use for services"
-  default     = "ip-range-svc"
+  default     = "gke-argocd-ip-range-svc"
 }
 
