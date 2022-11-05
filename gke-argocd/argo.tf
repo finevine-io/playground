afeeb1547f0505fff37b6c7bfde7973176839c08
Jose Asuncion <jeunito@gmail.com>
2022-11-04 17:02:14 -0700

    add argocd

---
+++ gke-argocd/argo.tf
@@ -0,0 +1,14 @@
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.endpoint}"
+    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+}
